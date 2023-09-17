59b5f49bd3e02122d7c4364f278e4a266e95c95c
David Ortiz <david.ortiz.p@gmail.com>
2023-09-17 16:36:37 -0500

    change get transactions from api

--- src/util/util.js
+++ src/util/util.js
@@ -1,13 +1,23 @@
 import { transactions } from "../data"
 
+const port = "9000"
+const host = "http://localhost"
+
+
 export function getTransaction (id) {
     const result = transactions.filter( transaction => transaction.id === id)
 
     if (result) return result[0]
 }
 
-export function getTransactions() {
-    return new Promise((resolve, reject) => {
-        resolve(transactions)
-    })
+export async function getTransactions() {
+    // return new Promise((resolve, reject) => {
+    //     resolve(transactions)
+    // })
+    const endpoint = "transactions"
+    const url = `${host}:${port}/${endpoint}`  
+    console.log(url)
+    const response = await fetch(url)
+    const transactions = await response.json()
+    return transactions
 }
